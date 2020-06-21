69b3608c658dafb5bd9e5160bb403a9e0eab1fda
Petri Kallio <petri.kallio@hel.fi>
2020-06-21 14:49:53 +0300

    progress

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -2,6 +2,35 @@
 #include <stdint.h>
 #include <string.h>
 #include "parser.h"
+#include "util/binary.h"
+
+typedef struct _frame_target {
+    char* frame_id;
+    long tag_offset;
+} FrameTarget;
+
+const FrameTarget frame_targets[] = {
+    {.frame_id = "TALB", .tag_offset = (long)offsetof(ID3Tags, album)},
+    {.frame_id = "TPE1", .tag_offset = (long)offsetof(ID3Tags, artist)},
+    {.frame_id = "TIT2", .tag_offset = (long)offsetof(ID3Tags, title)},
+};
+
+long* get_tag_offset(char* label)
+{
+    long* offset = NULL;
+
+    for (unsigned int i = 0; i < 4; i++) {
+        if (!memcmp(label, frame_targets[i].frame_id, 4)) {
+            offset = malloc(sizeof(long));
+
+            *offset = frame_targets[i].tag_offset;
+
+            break;
+        }
+    }
+
+    return offset;
+}
 
 int has_v2_tag(FILE* mp3file)
 {
@@ -24,17 +53,50 @@ void parse_v2_tag(ID3Tags* tags, FILE* mp3file)
 {
     uint16_t version;
     unsigned char flags;
-    uint32_t tag_size;
+    char _tag_size[] = {0, 0, 0, 0};
 
     fread(&version, sizeof(uint16_t), 1, mp3file);
     fread(&flags, sizeof(unsigned char), 1, mp3file);
-    fread(&tag_size, sizeof(uint32_t), 1, mp3file);
+    fread(&_tag_size, sizeof(char), 4, mp3file);
     
     if (feof(mp3file)) {
         return;
     }
 
+    // each byte has 7 bits that count
+    unsigned int tag_size = remove_padding(_tag_size, 4, 7); 
 
+    unsigned int i = 0;
+
+    while (i < tag_size && !feof(mp3file)) {
+        char frame_id[] = {0, 0, 0, 0};
+        uint32_t frame_size;
+        char flags[] = {0, 0};
+
+        fread(&frame_id, sizeof(char), 4, mp3file);
+        fread(&frame_size, sizeof(uint32_t), 1, mp3file);
+        fread(&flags, sizeof(char), 2, mp3file);
+        
+        if (feof(mp3file)) {
+            return;
+        }
+
+        long* tag_offset = get_tag_offset(frame_id);
+
+        if (tag_offset != NULL) {
+            fseek(mp3file, 1, SEEK_CUR);
+
+            tags[*tag_offset] = calloc(sizeof(char), frame_size);
+
+            fread(tags[*tag_offset], sizeof(char), frame_size - 1, mp3file);
+
+            free(tag_offset);
+        } else {
+            fseek(mp3file, frame_size, SEEK_CUR);
+        }
+
+        i += frame_size + 10; // frame size + header size
+    }
 }
 
 ID3Tags* parseMP3(FILE* mp3file)
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,14 +1,15 @@
-#ifndef MP3_HEADER_EDITOR_PARSER_H
-#define MP3_HEADER_EDITOR_PARSER_H
+#ifndef _ID3_TAG_EDITOR_PARSER_H
+#define _ID3_TAG_EDITOR_PARSER_H
 
 #include <stdio.h>
 
 typedef struct _id3_tags {
     char* artist;
     char* title;
+    char* album;
     unsigned int* track;
 } ID3Tags;
 
 ID3Tags* parseMP3(FILE* mp3file);
 
-#endif /* MP3_HEADER_EDITOR_PARSER_H */
+#endif /* _ID3_TAG_EDITOR_PARSER_H */
